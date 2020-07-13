7c504890a024db6edb4d8748940fbcc715aab970
IamPuni-7 <Indianpunith@gmail.com>
2020-07-13 18:42:23 +0530

    Second commit

--- src/main/java/com/example/demo/CompanyRepository.java
+++ src/main/java/com/example/demo/CompanyRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CompanyRepository extends CrudRepository<Company, Integer> {
+	
+	@Modifying
+    @Query("Select Contact from Company C WHERE c.name = :name")
+    Iterable<Contact> getAllContacts(@Param("name") String name);
 
 }
--- src/main/java/com/example/demo/MyRESTController.java
+++ src/main/java/com/example/demo/MyRESTController.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
-import java.net.http.HttpResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +14,9 @@ public class MyRESTController {
 
 	@Autowired
 	ContactRepository repository;
+	
+	@Autowired
+	CompanyRepository companyRepo;
 
 	@GetMapping("/contacts")
 	public Iterable<Contact> getContacts() {
@@ -36,5 +36,15 @@ public class MyRESTController {
 		
 		return repository.updateAddress(email, name);
 	}
-
+	
+	@DeleteMapping("/contacts/delete")
+	public void deleteContact(Contact contact)
+	{
+		repository.delete(contact);
+	}
+	
+	@GetMapping("/contacts/company_name")
+	public Iterable<Contact> getContactsByCompanyName(@Param("company_name") String name) {
+		return companyRepo.getAllContacts(name);
+	}
 }
